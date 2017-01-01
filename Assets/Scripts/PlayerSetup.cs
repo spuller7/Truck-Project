ff23b972976077885f1dd0e093f1cbd3f8f95b23
jakespuller <jake.spuller@afit.edu>
2017-01-01 10:19:51 -0500

    still fixing multiple car problem

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSetup : Photon.MonoBehaviour
+{
+
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+
+    void Start()
+    {
+        DisableComponents();
+        //Here you would call a function to assign the camera to follow this ship because every camera is unique to the local player
+
+    }
+    void DisableComponents()
+    {
+        for (int i = 0; i < componentsToDisable.Length; i++)
+        {
+            if (photonView.isMine == false)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+    }
+}
