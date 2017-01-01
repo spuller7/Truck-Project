6bd6fae294e676b40b87f47784be1042adea9134
jakespuller <jake.spuller@afit.edu>
2017-01-01 03:17:09 -0500

    users can still control multiple cars

--- Assets/Scripts/TEST.cs
+++ Assets/Scripts/TEST.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 
 public class TEST : Photon.MonoBehaviour {
 
-    GameObject test;
+    string spawn;
+    bool gameStart = false;
+
     // Use this for initialization
     void Start () {
-	}
+       // GameObject myCar = PhotonNetwork.Instantiate("CamaroSS69_DOOM", Vector3.zero, Quaternion.identity, 0);
+       // Camera.main.GetComponent<CameraFollow>().target = myCar.transform;
+
+        
+
+        if (PhotonNetwork.player.NickName != "Jake")
+        {
+            spawn = "Spawn6";
+        }
+        else
+        {
+            spawn = "Spawn7";
+        }
+         //GameObject myCar = PhotonNetwork.Instantiate("CamaroSS69_DOOM", GameObject.Find(spawn).transform.position, Quaternion.identity, 0);
+         //Camera.main.GetComponent<CameraFollow>().target = myCar.transform;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown("space"))
+		if (Input.GetKeyDown("space") && PhotonNetwork.room.PlayerCount == 2)
         {
             GameObject myCar = PhotonNetwork.Instantiate("CamaroSS69_DOOM", Vector3.zero, Quaternion.identity, 0);
             Camera.main.GetComponent<CameraFollow>().target = myCar.transform;
+        }
 
-            //    test = GameObject.Find("CamaroSS69_DOOM");
-            //    Instantiate(test, Vector3.zero, Quaternion.identity);
+        if (PhotonNetwork.room.PlayerCount == 2 && !gameStart)
+        {
+            GameObject myCar = PhotonNetwork.Instantiate("CamaroSS69_DOOM", GameObject.Find(spawn).transform.position, Quaternion.identity, 0);
+            Camera.main.GetComponent<CameraFollow>().target = myCar.transform;
+            gameStart = true;
         }
 	}
+
 }
